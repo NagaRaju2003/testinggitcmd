bab3889588724045a303eb56b10d30e8d1eca37c
nagarajuk1 <nagaraju.k@releaseowl.com>
2025-04-30 17:06:23 +0530

    commiting latest changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
-
+        System.out.println("newqa");
     }
 }
