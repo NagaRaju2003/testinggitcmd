7fb41dd7e6b8f7668736bf5b686ad1f48ec66784
anudeepika.a <anudeepika.a@releaseowl.com>
2025-04-30 17:08:11 +0530

    Anu's Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("newqa");
+        System.out.println("Anu hII");
     }
 }
